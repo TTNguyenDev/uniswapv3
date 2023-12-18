ae8868900133d66778069581589b77897a8b5b60
TTNguyenDev <nttriet9a4@gmail.com>
2023-12-18 22:56:50 +0700

    Flashloan fee

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -29,6 +29,7 @@ contract UniswapV3Pool is IUniswapV3Pool {
     error NotEnoughLiquidity();
     error InvalidPriceLimit();
     error AlreadyInitialized();
+    error FlashLoanNotPaid();
 
     event Mint(
         address sender,
@@ -526,16 +527,25 @@ contract UniswapV3Pool is IUniswapV3Pool {
         uint256 amount1,
         bytes calldata data
     ) public {
+        uint256 fee0 = Math.mulDivRoundingUp(amount0, fee, 1e6);
+        uint256 fee1 = Math.mulDivRoundingUp(amount1, fee, 1e6);
+
         uint256 balance0Before = IERC20(token0).balanceOf(address(this));
         uint256 balance1Before = IERC20(token1).balanceOf(address(this));
 
         if (amount0 > 0) IERC20(token0).transfer(msg.sender, amount0);
         if (amount1 > 0) IERC20(token1).transfer(msg.sender, amount1);
 
-        IUniswapV3FlashCallback(msg.sender).uniswapV3FlashCallback(data);
+        IUniswapV3FlashCallback(msg.sender).uniswapV3FlashCallback(
+            fee0,
+            fee1,
+            data
+        );
 
-        require(IERC20(token0).balanceOf(address(this)) >= balance0Before);
-        require(IERC20(token1).balanceOf(address(this)) >= balance1Before);
+        if (IERC20(token0).balanceOf(address(this)) < balance0Before + fee0)
+            revert FlashLoanNotPaid();
+        if (IERC20(token1).balanceOf(address(this)) < balance1Before + fee1)
+            revert FlashLoanNotPaid();
 
         emit Flash(msg.sender, amount0, amount1);
     }
--- src/interfaces/IUniswapV3FlashCallback.sol
+++ src/interfaces/IUniswapV3FlashCallback.sol
@@ -2,5 +2,9 @@
 pragma solidity ^0.8.14;
 
 interface IUniswapV3FlashCallback {
-    function uniswapV3FlashCallback(bytes memory data) external;
+    function uniswapV3FlashCallback(
+        int24 fee0,
+        int24 fee1,
+        bytes memory data
+    ) external;
 }
